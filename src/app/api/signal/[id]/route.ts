1593d6e8f33ef6663b007c04421fe88f669b7a0f
gavinarori <99688077+gavinarori@users.noreply.github.com>
2024-04-09 11:34:05 +0300

    added(UI):for the distress

--- src/app/api/signal/[id]/route.ts
+++ src/app/api/signal/[id]/route.ts
@@ -1,18 +1,36 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 import { main } from "../route";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    await main();
 
 
-  export const GET = async (req: Request, res: NextResponse) => {
-    try {
-      await main();
-      const getLocation = await prisma.location.findFirst();
-      if (!getLocation)
-        return NextResponse.json({ message: "Not Found" }, { status: 404 });
-      return NextResponse.json({ message: "Success", getLocation }, { status: 200 });
-    } catch (err) {
-      return NextResponse.json({ message: "Error", err }, { status: 500 });
-    } finally {
-      await prisma.$disconnect();
-    }
-  };
+    
+    const currentUser = await getCurrentUser();
+
+    // Validate userId, if necessary
+
+    // Fetch user's location based on userId
+    const getLocation = await prisma.location.findFirst({
+      where: {
+        userId: currentUser.id , // Assuming userId is a string
+      },
+    });
+
+    // Check if location is found
+    if (!getLocation)
+      return NextResponse.json({ message: "Location not found" }, { status: 404 });
+
+    // Return the location
+    return NextResponse.json({ message: "Success", location: getLocation }, { status: 200 });
+  } catch (err) {
+    // Handle errors
+    return NextResponse.json({ message: "Error", error: err }, { status: 500 });
+  } finally {
+    // Disconnect from Prisma
+    await prisma.$disconnect();
+  }
+};
