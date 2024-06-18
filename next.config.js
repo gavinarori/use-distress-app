9f014f6c37e0fd312d5793d7bce8a57a905bbbaa
gavinarori <arorigavin@gmail.com>
2024-06-18 09:58:10 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  cacheOnFrontEndNav:true,
-  aggressiveFrontEndNavCaching:true,
-  reloadOnOnline:true,
-  swcMinify:true,
-  disable:false,
-  workboxOptions:{
-    disableDevLogs:true,
-  },
 });
 
 const nextConfig =  {
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,17 @@
+import { PrismaClient } from '@prisma/client'
 
-import { PrismaClient } from "@prisma/client"
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query']
+  })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
